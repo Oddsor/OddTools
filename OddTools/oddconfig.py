b881f45dbad9efb37d797e0b0220fe42f948e885
Odd Andreas Sørsæther <osorseth@gmail.com>
2015-01-09 13:07:06 +0100

    Errors getting keys will also show list of settings-files read.
    Signed-off-by: Odd Andreas Sørsæther <osorseth@gmail.com>

--- OddTools/oddconfig.py
+++ OddTools/oddconfig.py
@@ -10,19 +10,22 @@ class OddConfig():
         self.config.read(path)
 
 config = configparser.ConfigParser()
-config.read('settings.ini')
+read_files = list()
+read_files.extend(config.read('settings.ini'))
 
 if "DEFAULT" not in config:
     config.add_section("DEFAULT")
 
+
 def read(path):
-    config.read(path)
+    read_files.extend(config.read(path))
+
 
 def get_setting(key, section='DEFAULT'):
     if config.has_option(section, key):
         return config[section][key]
     else:
-        raise Exception("Key " + key + " not found in config file.")
+        raise Exception("Key " + key + " not found in config file. " + str(read_files))
 
 
 def set_setting(key, value, section='DEFAULT'):
